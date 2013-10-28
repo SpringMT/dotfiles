66aaf92673d7c384a3ce8fee4ceaebe97866f7e3
haruyama-makoto <makoto.haruyama@dena.com>
2013-10-28 10:57:25 +0900

    Update for boxen

--- setup.sh
+++ setup.sh
@@ -2,18 +2,11 @@
 
 # global setting
 cd
-/usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/gist/323731)"
-brew install git
-brew install lv
-brew install gist
-brew install libyaml
-git clone git@github.com:SpringMT/dotfiles.git
 
 cat <<EOF > ~/.bashrc
 if [ -f $HOME/dotfiles/.bashrc ]; then
     source $HOME/dotfiles/.bashrc
 fi
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 EOF
 
 cat <<EOF > ~/.vimrc
